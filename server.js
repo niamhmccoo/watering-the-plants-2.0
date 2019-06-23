310986cd6d025d3e6328c792f7fc16035ac4dc01
niamhmccoo <niamhmccooey@gmail.com>
2019-06-23 15:51:03 +0100

    Added app.post for "/create-plant"

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 let express = require("express");
 let app = express();
+let Database = require("nedb");
 
-app.listen(3000, function() {
+let database = new Database({
+    filename: __dirname + "/database/plants.json",
+    autoload: true
+});
+
+app.listen(3000, function () {
     console.log("Server is listening & ready on port 3000.");
 });
 
 app.use(express.static("public"));
+
+app.post("/create-plant", function (req, res) {
+    console.log("Yay plants!");
+});
