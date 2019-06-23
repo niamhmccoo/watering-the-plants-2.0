c7c7396bbbd8b8874bf7516ea8798c2da66d27ba
niamhmccoo <niamhmccooey@gmail.com>
2019-06-23 16:14:49 +0100

    app.get "/get-plants" + jQuery & JS scripts html

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 let express = require("express");
 let app = express();
 let Database = require("nedb");
-
 let database = new Database({
     filename: __dirname + "/database/plants.json",
     autoload: true
@@ -14,5 +13,14 @@ app.listen(3000, function () {
 app.use(express.static("public"));
 
 app.post("/create-plant", function (req, res) {
+    // database.insert({name: req.fields.plantText}, function(error, result) {
+    //     res.redirect("/");
+    // })
     console.log("Yay plants!");
+});
+
+app.get("/get-plants", function (req, res) {
+    database.find({}).sort({ name: 1}).exec(function (err,docs) {
+        console.log("Plant Party!");
+    });
 });
