1861acdabd6c686dfb421e9109bd9fa843fe4176
niamhmccoo <niamhmccooey@gmail.com>
2019-06-23 16:41:20 +0100

    Added app.post for "/water-plants"

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ let app = express();
 let formidable = require("express-formidable");
 let fs = require("fs");
 let Database = require("nedb");
-let database = new Database({
-    filename: __dirname + "/database/plants.json",
-    autoload: true
-});
 
 app.listen(3000, function () {
     console.log("Server is listening & ready on port 3000.");
 });
 
 app.use(express.static("public"));
+
+let database = new Database({
+    filename: __dirname + "/database/plants.json",
+    autoload: true
+});
+
+app.post("/water-plants", function (req, res) {
+    console.log("Watered!");
+});
 
 app.post("/create-plant", function (req, res) {
     console.log("Yay plants!");
