56f3c789e11420257f4974b981e210871910f0ac
niamhmccoo <niamhmccooey@gmail.com>
2019-06-23 16:46:19 +0100

    Time formatting & refreshing functions

--- public/main.js
+++ public/main.js
@@ -14,3 +14,31 @@ function setupPlant(plant) {
         </div>
       </div>`))
 };
+
+function formatTimeAgo(timeInMilliseconds) {
+    let timeInS = Math.round(timeInMilliseconds / 1000);
+    let secondsOnly = timeInS % 60;
+    let timeInM = (timeInS - secondsOnly) / 60;
+    let minutesOnly = timeInM % 60;
+    let timeInH = (timeInM - minutesOnly) / 60;
+    let hoursOnly = timeInH % 24;
+    let timeInD = (timeInH - hoursOnly) / 24;
+    return `${timeInD}d ${timeInH}h ${minutesOnly}m ${secondsOnly}s ago`;
+}
+
+function refreshTimes() {
+    $("time").each(function () {
+        let watered = this.dateTime;
+
+        if (watered) {
+            wateredText = formatTimeAgo(+(new Date()) - +(Date.parse(watered)));
+        } else {
+            wateredText = "Never watered";
+        }
+
+        console.log("Please refresh", this, wateredText)
+        $(this).text(wateredText)
+    });
+};
+
+setInterval(refreshTimes, 1000);
