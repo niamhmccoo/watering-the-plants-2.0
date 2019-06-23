04e991d761ca9e3a68b05cb8d26c46b82b441834
niamhmccoo <niamhmccooey@gmail.com>
2019-06-23 16:16:26 +0100

    Added app.get ofr "/get-plants"

--- server.js
+++ server.js
@@ -13,14 +13,9 @@ app.listen(3000, function () {
 app.use(express.static("public"));
 
 app.post("/create-plant", function (req, res) {
-    // database.insert({name: req.fields.plantText}, function(error, result) {
-    //     res.redirect("/");
-    // })
     console.log("Yay plants!");
 });
 
 app.get("/get-plants", function (req, res) {
-    database.find({}).sort({ name: 1}).exec(function (err,docs) {
-        console.log("Plant Party!");
-    });
+    console.log("Plant Party!");
 });
